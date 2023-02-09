db6d8446a5ad1c5960701a155fb49f2360e39c55
DareNamibia <122939810+DareNamibia@users.noreply.github.com>
2023-02-09 10:19:14 +0200

    updated js

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-
+// const navbar = document.getElementById('navbar')
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-
+  navbar.classList.remove('top');
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
